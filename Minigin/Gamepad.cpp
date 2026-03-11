1ed6d99b3108457f50ae8a5355eb0c41c5c3f246
GiselleHarnevie <giselleharnevie12@gmail.com>
2026-03-11 23:36:13 +0100

    - empty gamepad

---
+++ Minigin/Gamepad.cpp
@@ -0,0 +1,2 @@
+#include "Gamepad.h"
+//#include <Xinput.h>
---
+++ Minigin/Gamepad.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class Gamepad
+{
+public:
+	/*Gamepad();
+	~Gamepad();
+
+	bool IsDownThisFrame(unsigned int button) const;
+	bool IsUpThisFrame(unsigned int button) const;
+	bool IsPressed(unsigned int button) const;*/
+private:
+
+};
